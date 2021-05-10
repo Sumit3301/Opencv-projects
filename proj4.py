2063b5645c9bb4cd56b6b232fe170ea169c6bfbe
Sumit Gaur <sumit.gaur1999@gmail.com>
2021-05-10 19:22:33 +0530

    different shapes in opencv

--- proj4.py
+++ proj4.py
@@ -1,10 +1,18 @@
-#drawing shapes
+#drawing shapes and blank boxes
 import cv2
 import numpy as np
 blank=np.zeros((200,200,3),dtype='uint8')
 cv2.imshow('blank',blank)
 
 blank[:]=255,255,255
-cv2.imshow('green',blank)
+
+cv2.rectangle(blank,(0,0),(blank.shape[1]//2,blank.shape[0]//2),(0,255,0),thickness=cv2.FILLED)
+cv2.imshow('rectangle',blank)
+
+cv2.circle(blank,(blank.shape[1]//2,blank.shape[0]//2),20,(0,0,255),thickness=-1)
+cv2.imshow('circle',blank)
+
+cv2.line(blank,(0,0),(blank.shape[1]//2,blank.shape[0]//2),(255,255,255),thickness=2)
+cv2.imshow('line',blank)
 
 cv2.waitKey(0)
