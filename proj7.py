4ab0cb520e5a2073f665fd11613acc5e89c3e96c
Sumit Gaur <sumit.gaur1999@gmail.com>
2021-05-12 19:51:01 +0530

    Contour Detection

--- proj7.py
+++ proj7.py
@@ -3,12 +3,13 @@ import cv2
 img=cv2.imread('man.jpg')
 resized_image=cv2.resize(img,(img.shape[1]//5,img.shape[1]//6),interpolation= cv2.INTER_AREA)
 cv2.imshow('orignal',resized_image)
-
+blur=cv2.GaussianBlur(resized_image,(3,3),cv2.BORDER_DEFAULT)
+cv2.imshow('Blur',blur)
 gray=cv2.cvtColor(resized_image,cv2.COLOR_BGR2GRAY)
 cv2.imshow('Grey Image',gray)
-canny=cv2.Canny(resized_image,125,175)
+canny=cv2.Canny(blur,125,175)
 cv2.imshow('Canny',canny)
 
-contours,heiarchies = cv2.findContours(canny,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-
+contours,heiarchies = cv2.findContours(canny,cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
+print(len(contours))
 cv2.waitKey(0)
