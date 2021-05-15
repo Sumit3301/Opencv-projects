af4f1476e3830032f541501d680924d6652211bc
Sumit Gaur <sumit.gaur1999@gmail.com>
2021-05-15 15:02:37 +0530

    Gradiant and edge detetion

--- proj15.py
+++ proj15.py
@@ -14,4 +14,10 @@ lap=cv2.Laplacian(gray,cv2.CV_64F)
 lap=np.uint8(np.absolute(lap))
 cv2.imshow('Laplacian',lap)
 
+#sabel (gradiants to diretions)
+sobel_x=cv2.Sobel(gray,cv2.CV_64F,1,0)
+sobel_y=cv2.Sobel(gray,cv2.CV_64F,0,1)
+cv2.imshow('Sobel x',sobel_x)
+cv2.imshow('Sobel y',sobel_y)
+
 cv2.waitKey(0)
